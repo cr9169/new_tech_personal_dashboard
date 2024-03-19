1a3148389194f7441dc40096ebddffa251f094ac
cr9169 <bar.gabay.home@gmail.com>
2024-03-19 20:37:55 +0200

    feat: added mui icons for toolbar components

--- TODO.md
+++ TODO.md
@@ -7,3 +7,4 @@
     ### bank balance (in USD and EURO)
     ### crypto balance (in Bitcoin, Ethereum, Solana, Dogecoin)
     ### total nutritians goals for every day in this week (calories, protein, carbs, fat). will be in a table form
+
--- front/src/components/Dashboard/Toolbar/Toolbar.tsx
+++ front/src/components/Dashboard/Toolbar/Toolbar.tsx
@@ -1,5 +1,12 @@
-import React from "react";
 import "./Toobar.scss";
+import React from "react";
+import AppSettingsAltRoundedIcon from "@mui/icons-material/AppSettingsAltRounded";
+import CalendarMonthRoundedIcon from "@mui/icons-material/CalendarMonthRounded";
+import GroupsRoundedIcon from "@mui/icons-material/GroupsRounded";
+import WcRoundedIcon from "@mui/icons-material/WcRounded";
+import AccountBalanceRoundedIcon from "@mui/icons-material/AccountBalanceRounded";
+import CurrencyBitcoinRoundedIcon from "@mui/icons-material/CurrencyBitcoinRounded";
+import RestaurantRoundedIcon from "@mui/icons-material/RestaurantRounded";
 
 const Toobar = ({}: {}) => {
   return <div></div>;
