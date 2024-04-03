3dabbe62f49eab0b9a26036e01006de5a6e418ec
cr9169 <bar.gabay.home@gmail.com>
2024-04-03 18:33:18 +0300

    feat: added name type writter

--- front/src/components/Dashboard/DataScreen/ComponentsSection/ComponentsSection.tsx
+++ front/src/components/Dashboard/DataScreen/ComponentsSection/ComponentsSection.tsx
@@ -1,8 +1,21 @@
-import React from "react";
 import "./ComponentsSection.scss";
+import { useTypewriter } from "react-simple-typewriter";
 
 const ComponentsSection = () => {
-  return <div className="components-section-main-box"></div>;
+  const [text] = useTypewriter({
+    words: ["Hi, Jack Black!!!"],
+    loop: false,
+    typeSpeed: 120,
+  });
+
+  return (
+    <div className="components-section-main-box">
+      <div className="components-section-content-wrapper">
+        <span className="user-name">{text}</span>
+        <span style={{ color: "red" }}></span>
+      </div>
+    </div>
+  );
 };
 
 export default ComponentsSection;
